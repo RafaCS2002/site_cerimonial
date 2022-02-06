09e2a1c01a55f8526d93e7048ae3794682eedac0
Rafael Sampaio <rafaelsampaio200802@gmail.com>
2022-02-06 18:00:49 -0300

    Nav Button working, could add a general generation method

--- src/components/navButton.js
+++ src/components/navButton.js
@@ -1,48 +1,17 @@
 import React from 'react';
-import styled from 'styled-components';
 import { FaBars, FaRegWindowClose as FaClose } from "react-icons/fa";
+import styled from 'styled-components';
+import { StyledNavUl, StyledNavLi, Opc } from '../../styles/navButtonStyle'; 
 
-
-const Holder = styled.div`
-    width:50%;
-    background-color:none;
-    display:inline-flex;
-    justify-content:space-evenly;
-    align-items: end;
-    padding-bottom:30px;
-    @media (max-width:600px){
-        padding-bottom:25px;
-    }
-    
-`
-
-function Option(props) {
-    const txt = props.children;
-    const Opc = styled.a`
-            display:flex;
-            all:unset;
-            &:hover{
-                color:#4e5965;
-            }
-            @media (max-width:600px){
-                display:none;
-            }
-    `
-    return (
-        <>
-            <Opc href={props.link}>{txt}</Opc>
-        </>
-    )
-}
-
-function ButtonOpc({ children }) {
+export function NavButton() {
+    const [isOpen, setIsOpen] = React.useState(false);
+    let icon = null;
 
     if (isOpen === false) {
         icon = FaBars;
     } else {
         icon = FaClose;
     }
-
     const Bars = styled(icon)`
         @media (min-width:600px){
             display:none;
@@ -53,37 +22,32 @@ function ButtonOpc({ children }) {
     return (
         <>
             <Bars onClick={() => {
-                if (isOpen === false) {
-                    setIsOpen(true);
-                } else {
+                if (isOpen) {
+                    console.log('eh as barras q tao aparecendo')
                     setIsOpen(false);
+                } else {
+                    console.log('eh o X q ta aparecendo')
+                    setIsOpen(true);
                 }
-                console.log({ isOpen })
             }} />
-            {isOpen===true?(
-                <ul>
-                    <li link={'#'}>Sobre Nos</li>
-                    <li link={'#'}>Trabalhos</li>
-                    <li link={'#'}>Metodo</li>
-                    <li link={'#'}>Aliados</li>
-                </ul>
-            ):()}
-            
-        </>
-    )
-}
 
+            {isOpen && (
+                <StyledNavUl>
+                    <StyledNavLi>
+                        <Opc href='#' onClick={() => setIsOpen(false)}>Sobre Nos</Opc>
+                    </StyledNavLi>
+                    <StyledNavLi>
+                        <Opc href='#' onClick={() => setIsOpen(false)}>Trabalhos</Opc>
+                    </StyledNavLi>
+                    <StyledNavLi>
+                        <Opc href='#' onClick={() => setIsOpen(false)}>Metodo</Opc>
+                    </StyledNavLi>
+                    <StyledNavLi>
+                        <Opc href='#' onClick={() => setIsOpen(false)}>Aliados</Opc>
+                    </StyledNavLi>
+                </StyledNavUl>
+            )}
 
-export function NavButton() {
-    return (
-        <>
-            <Holder>
-                <Option link={'#'}>Sobre Nos</Option>
-                <Option link={'#'}>Trabalhos</Option>
-                <Option link={'#'}>Metodo</Option>
-                <Option link={'#'}>Aliados</Option>
-                <ButtonOpc />
-            </Holder>
         </>
     )
 }
---
+++ styles/navButtonStyle.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const StyledNavUl = styled.ul`
+position:absolute;
+top:11%;
+right:0%;
+width:100%;
+display:flex;
+flex-direction:column;
+justify-content:center;
+align-items:center;
+background-color:white;
+padding-top:10px;
+padding-bottom:10px;
+@media (min-width: 600px){
+    display:none;
+}
+`
+export const StyledNavLi = styled.li`
+display:block;
+margin-top:10px;
+margin-bottom:10px;
+`
+export const Opc = styled.a`
+display:flex;
+all:unset;
+&:hover{
+    color:#4e5965;
+}
+`
